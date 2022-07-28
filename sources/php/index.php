17fe63c8a6b94c6bdaafa566cf1bce4e47dfb617
kkarov <kkarov@jenkins.krkarov.com>
2022-07-28 11:10:59 -0400

    cleaning up

--- sources/php/index.php
+++ sources/php/index.php
@@ -4,17 +4,7 @@
 
 <h1>Hello, Netea!</h1></style>
 
-<?php 
-
-	$conn = new mysqlnd('localhost','root','UdxHGXatiHLiZmu1C71','classicmodels');
-	if($conn->connect_error){	
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		echo "nice";
-	}
-
-phpinfo() ?>
+<?php phpinfo() ?>
 
 
 </body>
